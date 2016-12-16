2f5b5f76523bb2206fb985cc942dc9aafee2b298
Suheel <yasins3@uni.coventry.ac.uk>
2016-12-16 22:32:20 +0000

    Create Question 7 Week 3.py

---
+++ Question 7 Week 3.py
@@ -0,0 +1,14 @@
+#   Week 3
+# Question 7
+# Write a recursive function (pseudocode and code) to check if a number n is prime
+
+PRIME (n)
+  if n is divisible by 2
+    return False
+  elif n == x
+    reuturn True
+  else:
+  
+    if n is divisible by x:
+        return False
+PRIME (n)
