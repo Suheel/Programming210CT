ae7fafbf405ca3ef1fc772cc7815765be1a99fa0
Suheel <yasins3@uni.coventry.ac.uk>
2016-12-16 22:49:42 +0000

    Create Question 8 Week 3.py

---
+++ Question 8 Week 3.py
@@ -0,0 +1,17 @@
+#Week 3
+#Question 8
+#Write a recursive function (pseudocode and code) that removes all vowels from a given
+#string s. Input: "beautiful" Output: "btfl".
+
+word = string("Enter Word")
+
+def vowel_remove:
+
+  if not word: #this code shows that there will be an empty string 
+      return word
+
+elif word[] in "aeiouAEIOU" # this line of code will show will display words if the first character of the word is a vowel
+  Return remove.vowels () # this line of code will then skip the first letter moving onto the next letters and so on removing the vowel
+
+print ("The vowels have been removed from the word")
+print (vowel remove)
