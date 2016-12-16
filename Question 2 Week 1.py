dddb6a26c7843ce4d7af505744e6906a28fd7180
Suheel <yasins3@uni.coventry.ac.uk>
2016-12-16 15:35:04 +0000

    Update Question 2 Week 1.py

--- Question 2 Week 1.py
+++ Question 2 Week 1.py
@@ -3,3 +3,22 @@
 #Count the number of trailing 0s in a factorial number 
 
 def trailing0s(factorialNumber)
+
+number=3
+answer = 2
+while factorialNumber>=number:
+  answer=answer*number
+  number=number+1
+  ## this piece of code will work out the answer of the factorial numbers
+  
+  
+  stringsolution=str(answer)
+  count=0
+  while stringsolution[1]=="0"=:
+      count = count = count +1
+      stringsolution=stringsolution[]
+      
+      
+print ("the solution to trailing 0's is (cout)+str) 
+  
+  
