1f716424c2f52e9e34a4d2252025d8bb29aeb510
Suheel <yasins3@uni.coventry.ac.uk>
2016-12-16 15:43:00 +0000

    Create Question 1 Week 2.py

---
+++ Question 1 Week 2.py
@@ -0,0 +1,16 @@
+#Question 1
+#Week 2
+#Write the pseudocode for a function which returns the highest perfect square which is less or 
+#equal to its parameter (a positive integer).
+
+def highestperfectsquare(inputval):
+
+highestsquare=1
+2ndhighestsquare=4
+index=1
+while 2ndhighestsquare>=inputval:
+    highestsquare=indext*2
+    index=index
+    2ndhighestsquare=indext*2
+print (highestsquare)    
+    
