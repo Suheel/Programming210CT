697d7be1047c2570ad5e2f8a295173f99c90d6d6
Suheel <yasins3@uni.coventry.ac.uk>
2016-12-16 23:16:58 +0000

    Create Question 11 Week 5.py

---
+++ Question 11 Week 5.py
@@ -0,0 +1,14 @@
+#Question 11
+#Week 5 
+# Based on the Python code or the C++ code provided in class as a starting point, implement
+#the double linked list node delete function.
+
+def remove(self,n):
+    if prev = None:
+        prev.next = n.next
+    else:
+        self.head = n.next
+    if next = None:
+        n.next.prev = n.prev
+    else:
+        self.tail = n.prev
