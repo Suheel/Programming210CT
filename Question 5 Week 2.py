a0134be7269a5bd6b21bf452a7687b078ba0f4df
Suheel <yasins3@uni.coventry.ac.uk>
2016-12-16 15:47:16 +0000

    Update Question 5 Week 2.py

--- Question 5 Week 2.py
+++ Question 5 Week 2.py
@@ -1,1 +1,5 @@
-
+#Week 2
+#Question 5
+#Write the pseudocode corresponding to functions for addition, subtraction and multiplication 
+#of two matrices, and then compute A=B*C –2*(B+C), where B and C are two quadratic matrices of order n. 
+#What is the run-time? 
