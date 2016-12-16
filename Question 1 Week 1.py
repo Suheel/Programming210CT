a40371138d988738e02b1ee4042fdaace319f3a9
Suheel <yasins3@uni.coventry.ac.uk>
2016-12-16 15:20:00 +0000

    Update Question 1 Week 1.py

--- Question 1 Week 1.py
+++ Question 1 Week 1.py
@@ -8,4 +8,10 @@ imporr random
 
 
 for i in range(0, 15): #this is the loop starting from 0 to the length of the array which is 15
+         p = random.radiant (0, i)) # here i have created a temporary list of the random numbers from the loop
+         if p == i: # this line is saying that if the value is the same then it should be created again
+                  continue
+          array[p], array[p] = array[p], array[i] # this line of code replaces the temporary list created above and will be replace it with the actual array
+          
+          print (array)
   
