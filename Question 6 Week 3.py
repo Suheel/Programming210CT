ff0c2563c3dffdf52a9c4d3ba74d14e7b24c0830
Suheel <yasins3@uni.coventry.ac.uk>
2016-12-16 22:24:39 +0000

    Create Question 6 Week 3.py

---
+++ Question 6 Week 3.py
@@ -0,0 +1,24 @@
+# Week 3
+# Question 6
+#Write the pseudocode and code for a function that reverses the words in a sentence. Input:
+#"This is awesome" Output: "awesome is This". Give the Big O notation.
+
+text= "This is awesome"
+text = text.split ('  ') # this line of code splits the words of a sentence into seperate words
+i = len
+text = ''
+while i > = 0
+     text = words[] # this code will then print the sentence in reverse order
+     i = 1
+print (text)
+
+
+# Pseudocode
+
+#TEXT < -- INPUT TEXT
+# WORDS < -- (TEXT) SPLIT
+# INDEX = LENGTH
+# TEXT < -- ()
+# WHILE < -- INDEX > = 0
+#PRINT TEXT
+# THE BIG O NOTATION FOR THIS QUESTION IS 0.
