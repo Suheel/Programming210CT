f47ca550430c827136a3b03fc01e69338eeaa52f
Suheel <yasins3@uni.coventry.ac.uk>
2016-12-16 23:13:06 +0000

    Create Question 10 Week 5.py

---
+++ Question 10 Week 5.py
@@ -0,0 +1,15 @@
+#Question 10
+#Week 5
+#Given a sequence of n integer numbers, extract the sub-sequence of maximum length
+#which is in ascending order
+
+array = [1,2,3,4,2,5,1,2,7]
+elist = []
+
+def seq(array,elist):
+    for i in range(0, len(array)1):
+        elist.append(array[])
+            return elist
+
+print("Starting List:",array)
+print("Longest SubSequence:",seq(array ,elist,))
