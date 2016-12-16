bfaf084d197c743361da9088b88698682ad648b6
Suheel <yasins3@uni.coventry.ac.uk>
2016-12-16 16:00:03 +0000

    Update Question 5 Week 2.py

--- Question 5 Week 2.py
+++ Question 5 Week 2.py
@@ -3,3 +3,19 @@
 #Write the pseudocode corresponding to functions for addition, subtraction and multiplication 
 #of two matrices, and then compute A=B*C –2*(B+C), where B and C are two quadratic matrices of order n. 
 #What is the run-time? 
+
+def AddMatrices(A,B):
+  answer=[]
+  # the function above will add the two matrices together
+  
+  if len(A) == len(B) and len(A=[0]) == len (B=[0]):
+  #THIS CODE WILL NOW CHECK IF THE DIMENSIONS OF THE MATRICES MATCH
+  
+  
+  For i in range (A=[0])
+  ROW []
+  for x in range (len(a=[0])):
+    row.append((A[i]+B[i])
+    answer.append.row
+               
+print(answer)               
